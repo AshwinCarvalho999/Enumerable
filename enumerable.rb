c1047d416b3dd7d90b4b1dbe703f4329f379c692
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-12-15 14:43:55 +0530

    Created our own Enumerable

---
+++ enumerable.rb
@@ -0,0 +1,19 @@
+module MyEnumerable
+  def all?
+    arr = []
+    list.each { |el| arr.push(el) if yield el }
+    puts arr.length == list.length
+  end
+
+  # def any?
+  #   arr = []
+  #   list.each { |el| arr.push(el) if yield el }
+  #   puts !arr.empty?
+  # end
+
+  # def filter
+  #   arr = []
+  #   list.each { |el| arr.push(el) if yield el }
+  #   puts arr
+  # end
+end
