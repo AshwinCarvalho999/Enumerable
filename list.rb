b9bac73a80590211bfee77214c39f064d22fc41c
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-12-15 15:10:13 +0530

    fixed some major bugs

--- list.rb
+++ list.rb
@@ -2,21 +2,22 @@ require_relative './enumerable'
 
 class MyList
   include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
 
-#   attr_reader :list
-#   def each
-#     yield @list
-#   end
-# end
+  attr_reader :list
 
-# numbers = MyList.new(1, 2, 3, 4)
-# numbers.each { |number| puts number }
-# numbers.all? { |number| number < 5 }
-# numbers.all? { |number| number > 5 }
-# numbers.any? { |number| number == 2 }
-# numbers.any? { |number| number == 5 }
+  def each
+    yield @list
+  end
+end
 
-# numbers.filter(&:even?)
+numbers = MyList.new(1, 2, 3, 4)
+numbers.each { |number| puts number }
+numbers.all? { |number| number < 5 }
+numbers.all? { |number| number > 5 }
+numbers.any? { |number| number == 2 }
+numbers.any? { |number| number == 5 }
+numbers.filter(&:even?)
