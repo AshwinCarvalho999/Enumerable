95f7db0ed7bab5768a5201c4a59ab142369f3ec1
nahnahAJ <nahnahahjuwah@gmail.com>
2022-12-15 12:27:04 +0000

    Created enumerable for filter

--- enumerable.rb
+++ enumerable.rb
@@ -11,9 +11,9 @@ module MyEnumerable
     puts !arr.empty?
   end
 
-  # def filter
-  #   arr = []
-  #   list.each { |el| arr.push(el) if yield el }
-  #   puts arr
-  # end
+  def filter
+    arr = []
+    list.each { |el| arr.push(el) if yield el }
+    puts arr
+  end
 end
